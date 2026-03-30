d69386f26fc221f3f8fdb1669586d5efe556e3f3
rafael-the-dev <luislangabusiness@gmail.com>
2026-03-30 08:15:28 +0200

    Practing File and FileInfo classes

---
+++ files/Lesson1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\salc\C#\files\Lesson1\file1.txt";
+            string targetPath = @"C:\salc\C#\files\Lesson1\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                //fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines) {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
