a3047d2039f8e53e6ac829f1b9bb9a30f8cac0ac
rafael-the-dev <luislangabusiness@gmail.com>
2026-03-30 08:00:15 +0200

    Learning and practing exceptions

---
+++ exceptions/Reservation/Entities/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Module.Entities
+{
+    sealed class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; private set; }
+        public DateTime CheckOut { get; private set; }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return "Room: " + RoomNumber
+                + "CheckIn: " + CheckIn.ToString("dd/MM/YYYY")
+                + "cHECKOut" + CheckOut.ToString("dd/MM/YYYY");
+        }
+    }
+}
