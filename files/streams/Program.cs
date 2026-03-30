4f592a2282b116dcc61fa81ab264d43f61fbdcd5
rafael-the-dev <luislangabusiness@gmail.com>
2026-03-30 08:32:48 +0200

    Instancing FileStream and StreamReader to process data from a file

---
+++ files/streams/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\salc\C#\files\streams\file1.txt";
+
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+
+                while(!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if(sr != null) sr.Close();
+            }
+        }
+    }
+}
