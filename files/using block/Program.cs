02a9a0d8514e2b73457402d4cdaf5c73d0272e66
rafael-the-dev <luislangabusiness@gmail.com>
2026-03-30 08:43:53 +0200

    testing using block

---
+++ files/using block/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"C:\salc\C#\files\using block\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            } catch(IOException e)
+            {
+                Console.WriteLine("An error occured"); 
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
