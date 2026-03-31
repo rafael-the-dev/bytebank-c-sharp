110598b200c86772fd413e1bfb230b186b2ab109
rafael-the-dev <luislangabusiness@gmail.com>
2026-03-31 08:10:27 +0200

    Use write stream to convert content from one file to another in uppercase

---
+++ files/WriteStream/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\salc\C#\files\WriteStream\file1.txt";
+            string targetPath = @"C:\salc\C#\files\WriteStream\file2.txt";
+
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            using (StreamWriter sw = File.AppendText(targetPath))
+            {
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line.ToUpper());
+                }
+            }
+        }
+    }
+}
