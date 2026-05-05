1e6142a0b8494d998662beb1a6719aef8d6c99a9
rafael-the-dev <luislangabusiness@gmail.com>
2026-05-05 21:14:41 +0200

    Learning Generics

---
+++ generics/Restrictions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Module.Entities;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculationService calculator = new CalculationService();
+        }
+    }
+}
---
+++ generics/Restrictions/entities/CalculationService.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace Module.Entities {
+	class CalculationService {
+		public T max<T>(T[] list) where T : IComperable {
+			T max = list[0];
+
+			for(let i = 1; i < list.Length; i++) {
+				const currentValue = list[i];
+
+				if(currentValue.CompareTo(max) > 0) {
+					max = currentValue;
+				}
+			}
+		}
+	}
+}
