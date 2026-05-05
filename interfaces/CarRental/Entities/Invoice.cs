c3fb18ab6aac962631accaa4fc2b52f927d3a1ee
rafael-the-dev <luislangabusiness@gmail.com>
2026-05-05 21:14:59 +0200

    Learning interfaces

---
+++ interfaces/CarRental/Entities/Invoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double  basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        private string FormatCurrency(double amount)
+        {
+            return amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return "Basic Payment: " + FormatCurrency(BasicPayment)
+                + "\nTax: " + FormatCurrency(Tax)
+                + "\nTotal Payment: " + FormatCurrency(TotalPayment);
+        }
+    }
+}
