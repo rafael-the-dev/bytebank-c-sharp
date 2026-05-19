69e00c6e8cd7b997311730320e906f4acb3e6e34
rafael-the-dev <luislangabusiness@gmail.com>
2026-05-19 20:29:54 +0200

    Create Invoker entity that will run or run a batch of commands in sequence

---
+++ classes/ByteBank/Invoker.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+using ByteBank.Interfaces;
+
+namespace ByteBank.Entities { 
+    public class Invoker {
+        private List<ICommand> history;
+
+        public Invoker() {
+           history = new List<ICommand>();
+        }
+
+        public void Run(ICommand command) {
+            command.Execute();
+
+            history.Add(command);
+        }
+
+        public void Rollback() {
+            sbyte length = (sbyte)history.Count();
+
+            while (length > 0) {
+                ICommand command = history.ElementAt(length - 1);
+
+                history.RemoveAt(length - 1);
+
+                command.Undo();
+            }
+        }
+
+        public void RunBatch(ICommand[] commands) { 
+            try { 
+                foreach(ICommand command in commands) {
+                    Run(command);
+                }
+            } catch(Exception e) {
+                Console.WriteLine("Erro encontrado, executando rollback...");
+
+                Rollback();
+            }
+        }
+    }
+}
