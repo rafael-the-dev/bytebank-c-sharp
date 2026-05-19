2606ac50dbeae3b639b97fa18c252f572b4f71d8
rafael-the-dev <luislangabusiness@gmail.com>
2026-05-19 21:21:21 +0200

    Create movement entity

--- classes/ByteBank/Movement.cs
+++ classes/ByteBank/Movement.cs
@@ -1,5 +1,7 @@
 using System;
 
+using ByteBank.Repositories;
+
 namespace ByteBank.Entities {
 	enum TYPE: sbyte { 
 		IN = 1,
@@ -8,11 +10,13 @@ namespace ByteBank.Entities {
 
 	class Movement {
 		public int Id { get; private set; }
+		public string AccountId { get; private set; }
 		public double Amount { get; private set; }
 		public TYPE Type { get; private set; }
 		public DateTime Date { get; private set; }
 
-		public Movement(int id, double amount, DateTime date, TYPE type) {
+		public Movement(int id, string accountId, double amount, DateTime date, TYPE type) {
+			AccountId = accountId;
 			Id = id;
 			Amount = amount;
 			Type = type;
